ed4d6b46f894634239efb0cc2f9c9528d31276cd
Cesar Augusto <cesardeazevedo@outlook.com>
2017-06-06 15:12:28 -0300

    Removed Generated Module

--- android/src/main/java/com/rnnestedscrollview/RNNestedScrollViewPackage.java
+++ android/src/main/java/com/rnnestedscrollview/RNNestedScrollViewPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNNestedScrollViewPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNNestedScrollViewModule(reactContext));
+      return Collections.emptyList();
     }
 
     @Override
