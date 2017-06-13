1ab095aa970ae1613ef413ce87854f517ed504f8
Cesar Augusto <cesardeazevedo@outlook.com>
2017-06-13 20:25:52 -0300

    Upgrade codebase from react-native 0.45-stable branch

--- android/src/main/java/com/rnnestedscrollview/ReactNestedScrollViewManager.java
+++ android/src/main/java/com/rnnestedscrollview/ReactNestedScrollViewManager.java
@@ -14,161 +14,219 @@ import javax.annotation.Nullable;
 import java.util.Map;
 
 import android.graphics.Color;
+import android.view.View;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
-import com.facebook.react.views.scroll.FpsListener;
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.uimanager.PixelUtil;
+import com.facebook.react.uimanager.Spacing;
+import com.facebook.react.uimanager.ViewProps;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
-import com.facebook.react.views.scroll.ScrollEventType;
 import com.facebook.react.uimanager.ReactClippingViewGroupHelper;
-import com.facebook.react.views.scroll.ReactScrollViewHelper;
+import com.facebook.react.uimanager.annotations.ReactPropGroup;
+import com.facebook.yoga.YogaConstants;
+import com.facebook.react.views.scroll.FpsListener;
+import com.facebook.react.views.scroll.ScrollEventType;
 import com.facebook.react.views.scroll.ReactScrollViewCommandHelper;
 
 /**
- * Forked from https://github.com/facebook/react-native/blob/v0.42.0/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollViewManager.java
+ * Forked from https://github.com/facebook/react-native/blob/0.45-stable/ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollViewManager.java
+ * View manager for {@link ReactNestedScrollView} components.
  *
- * View manager for {@link ReactScrollView} components.
- *
- * <p>Note that {@link ReactScrollView} and {@link ReactHorizontalScrollView} are exposed to JS
+ * <p>Note that {@link ReactNestedScrollView} and {@link ReactHorizontalScrollView} are exposed to JS
  * as a single ScrollView component, configured via the {@code horizontal} boolean property.
  */
+@ReactModule(name = ReactNestedScrollViewManager.REACT_CLASS)
 public class ReactNestedScrollViewManager
     extends ViewGroupManager<ReactNestedScrollView>
     implements ReactScrollViewCommandHelper.ScrollCommandHandler<ReactNestedScrollView> {
 
-    private static final String REACT_CLASS = "RCTNestedScrollView";
-    private @Nullable FpsListener mFpsListener = null;
+  protected static final String REACT_CLASS = "RCTNestedScrollView";
 
-    public ReactNestedScrollViewManager() {
-        this(null);
+  private static final int[] SPACING_TYPES = {
+      Spacing.ALL, Spacing.LEFT, Spacing.RIGHT, Spacing.TOP, Spacing.BOTTOM,
+  };
+
+  private @Nullable FpsListener mFpsListener = null;
+
+  public ReactNestedScrollViewManager() {
+    this(null);
+  }
+
+  public ReactNestedScrollViewManager(@Nullable FpsListener fpsListener) {
+    mFpsListener = fpsListener;
+  }
+
+  @Override
+  public String getName() {
+    return REACT_CLASS;
+  }
+
+  @Override
+  public ReactNestedScrollView createViewInstance(ThemedReactContext context) {
+    return new ReactNestedScrollView(context, mFpsListener);
+  }
+
+  @ReactProp(name = "scrollEnabled", defaultBoolean = true)
+  public void setScrollEnabled(ReactNestedScrollView view, boolean value) {
+    view.setScrollEnabled(value);
+  }
+
+  @ReactProp(name = "showsVerticalScrollIndicator")
+  public void setShowsVerticalScrollIndicator(ReactNestedScrollView view, boolean value) {
+    view.setVerticalScrollBarEnabled(value);
+  }
+
+  @ReactProp(name = ReactClippingViewGroupHelper.PROP_REMOVE_CLIPPED_SUBVIEWS)
+  public void setRemoveClippedSubviews(ReactNestedScrollView view, boolean removeClippedSubviews) {
+    view.setRemoveClippedSubviews(removeClippedSubviews);
+  }
+
+  /**
+   * Computing momentum events is potentially expensive since we post a runnable on the UI thread
+   * to see when it is done.  We only do that if {@param sendMomentumEvents} is set to true.  This
+   * is handled automatically in js by checking if there is a listener on the momentum events.
+   *
+   * @param view
+   * @param sendMomentumEvents
+   */
+  @ReactProp(name = "sendMomentumEvents")
+  public void setSendMomentumEvents(ReactNestedScrollView view, boolean sendMomentumEvents) {
+    view.setSendMomentumEvents(sendMomentumEvents);
+  }
+
+  /**
+   * Tag used for logging scroll performance on this scroll view. Will force momentum events to be
+   * turned on (see setSendMomentumEvents).
+   *
+   * @param view
+   * @param scrollPerfTag
+   */
+  @ReactProp(name = "scrollPerfTag")
+  public void setScrollPerfTag(ReactNestedScrollView view, String scrollPerfTag) {
+    view.setScrollPerfTag(scrollPerfTag);
+  }
+
+  /**
+   * When set, fills the rest of the scrollview with a color to avoid setting a background and
+   * creating unnecessary overdraw.
+   * @param view
+   * @param color
+   */
+  @ReactProp(name = "endFillColor", defaultInt = Color.TRANSPARENT, customType = "Color")
+  public void setBottomFillColor(ReactNestedScrollView view, int color) {
+    view.setEndFillColor(color);
+  }
+
+  /**
+   * Controls overScroll behaviour
+   */
+  @ReactProp(name = "overScrollMode")
+  public void setOverScrollMode(ReactNestedScrollView view, String value) {
+    view.setOverScrollMode(ReactNestedScrollViewHelper.parseOverScrollMode(value));
+  }
+
+  @Override
+  public @Nullable Map<String, Integer> getCommandsMap() {
+    return ReactScrollViewCommandHelper.getCommandsMap();
+  }
+
+  @Override
+  public void receiveCommand(
+      ReactNestedScrollView scrollView,
+      int commandId,
+      @Nullable ReadableArray args) {
+    ReactScrollViewCommandHelper.receiveCommand(this, scrollView, commandId, args);
+  }
+
+  @Override
+  public void scrollTo(
+      ReactNestedScrollView scrollView,
+      ReactScrollViewCommandHelper.ScrollToCommandData data) {
+    if (data.mAnimated) {
+      scrollView.smoothScrollTo(data.mDestX, data.mDestY);
+    } else {
+      scrollView.scrollTo(data.mDestX, data.mDestY);
+    }
+  }
+  @ReactPropGroup(names = {
+      ViewProps.BORDER_RADIUS,
+      ViewProps.BORDER_TOP_LEFT_RADIUS,
+      ViewProps.BORDER_TOP_RIGHT_RADIUS,
+      ViewProps.BORDER_BOTTOM_RIGHT_RADIUS,
+      ViewProps.BORDER_BOTTOM_LEFT_RADIUS
+  }, defaultFloat = YogaConstants.UNDEFINED)
+  public void setBorderRadius(ReactNestedScrollView view, int index, float borderRadius) {
+    if (!YogaConstants.isUndefined(borderRadius)) {
+      borderRadius = PixelUtil.toPixelFromDIP(borderRadius);
     }
 
-    public ReactNestedScrollViewManager(@Nullable FpsListener fpsListener) {
-        mFpsListener = fpsListener;
+    if (index == 0) {
+      view.setBorderRadius(borderRadius);
+    } else {
+      view.setBorderRadius(borderRadius, index - 1);
     }
+  }
 
-    @Override
-    public String getName() {
-        return REACT_CLASS;
-    }
+  @ReactProp(name = "borderStyle")
+  public void setBorderStyle(ReactNestedScrollView view, @Nullable String borderStyle) {
+    view.setBorderStyle(borderStyle);
+  }
 
-    @Override
-    public ReactNestedScrollView createViewInstance(ThemedReactContext context) {
-        return new ReactNestedScrollView(context, mFpsListener);
+  @ReactPropGroup(names = {
+      ViewProps.BORDER_WIDTH,
+      ViewProps.BORDER_LEFT_WIDTH,
+      ViewProps.BORDER_RIGHT_WIDTH,
+      ViewProps.BORDER_TOP_WIDTH,
+      ViewProps.BORDER_BOTTOM_WIDTH,
+  }, defaultFloat = YogaConstants.UNDEFINED)
+  public void setBorderWidth(ReactNestedScrollView view, int index, float width) {
+    if (!YogaConstants.isUndefined(width)) {
+      width = PixelUtil.toPixelFromDIP(width);
     }
+    view.setBorderWidth(SPACING_TYPES[index], width);
+  }
 
-    @ReactProp(name = "scrollEnabled", defaultBoolean = true)
-    public void setScrollEnabled(ReactNestedScrollView view, boolean value) {
-        view.setScrollEnabled(value);
-    }
+  @ReactPropGroup(names = {
+      "borderColor", "borderLeftColor", "borderRightColor", "borderTopColor", "borderBottomColor"
+  }, customType = "Color")
+  public void setBorderColor(ReactNestedScrollView view, int index, Integer color) {
+    float rgbComponent =
+        color == null ? YogaConstants.UNDEFINED : (float) ((int)color & 0x00FFFFFF);
+    float alphaComponent = color == null ? YogaConstants.UNDEFINED : (float) ((int)color >>> 24);
+    view.setBorderColor(SPACING_TYPES[index], rgbComponent, alphaComponent);
+  }
 
-    @ReactProp(name = "showsVerticalScrollIndicator")
-    public void setShowsVerticalScrollIndicator(ReactNestedScrollView view, boolean value) {
-        view.setVerticalScrollBarEnabled(value);
+  @Override
+  public void scrollToEnd(
+      ReactNestedScrollView scrollView,
+      ReactScrollViewCommandHelper.ScrollToEndCommandData data) {
+    // ScrollView always has one child - the scrollable area
+    int bottom =
+      scrollView.getChildAt(0).getHeight() + scrollView.getPaddingBottom();
+    if (data.mAnimated) {
+      scrollView.smoothScrollTo(scrollView.getScrollX(), bottom);
+    } else {
+      scrollView.scrollTo(scrollView.getScrollX(), bottom);
     }
+  }
 
-    @ReactProp(name = ReactClippingViewGroupHelper.PROP_REMOVE_CLIPPED_SUBVIEWS)
-    public void setRemoveClippedSubviews(ReactNestedScrollView view, boolean removeClippedSubviews) {
-        view.setRemoveClippedSubviews(removeClippedSubviews);
-    }
+  @Override
+  public @Nullable Map getExportedCustomDirectEventTypeConstants() {
+    return createExportedCustomDirectEventTypeConstants();
+  }
 
-    /**
-     * Computing momentum events is potentially expensive since we post a runnable on the UI thread
-     * to see when it is done.  We only do that if {@param sendMomentumEvents} is set to true.  This
-     * is handled automatically in js by checking if there is a listener on the momentum events.
-     *
-     * @param view
-     * @param sendMomentumEvents
-     */
-    @ReactProp(name = "sendMomentumEvents")
-    public void setSendMomentumEvents(ReactNestedScrollView view, boolean sendMomentumEvents) {
-        view.setSendMomentumEvents(sendMomentumEvents);
-    }
-
-    /**
-     * Tag used for logging scroll performance on this scroll view. Will force momentum events to be
-     * turned on (see setSendMomentumEvents).
-     *
-     * @param view
-     * @param scrollPerfTag
-     */
-    @ReactProp(name = "scrollPerfTag")
-    public void setScrollPerfTag(ReactNestedScrollView view, String scrollPerfTag) {
-        view.setScrollPerfTag(scrollPerfTag);
-    }
-
-    /**
-     * When set, fills the rest of the scrollview with a color to avoid setting a background and
-     * creating unnecessary overdraw.
-     * @param view
-     * @param color
-     */
-    @ReactProp(name = "endFillColor", defaultInt = Color.TRANSPARENT, customType = "Color")
-    public void setBottomFillColor(ReactNestedScrollView view, int color) {
-        view.setEndFillColor(color);
-    }
-
-    /**
-     * Controls overScroll behaviour
-     */
-    @ReactProp(name = "overScrollMode")
-    public void setOverScrollMode(ReactNestedScrollView view, String value) {
-        view.setOverScrollMode(ReactScrollViewHelper.parseOverScrollMode(value));
-    }
-
-    @Override
-    public @Nullable Map<String, Integer> getCommandsMap() {
-        return ReactScrollViewCommandHelper.getCommandsMap();
-    }
-
-    @Override
-    public void receiveCommand(
-        ReactNestedScrollView scrollView,
-        int commandId,
-        @Nullable ReadableArray args) {
-        ReactScrollViewCommandHelper.receiveCommand(this, scrollView, commandId, args);
-    }
-
-    @Override
-    public void scrollTo(
-        ReactNestedScrollView scrollView,
-        ReactScrollViewCommandHelper.ScrollToCommandData data) {
-        if (data.mAnimated) {
-            scrollView.smoothScrollTo(data.mDestX, data.mDestY);
-        } else {
-            scrollView.scrollTo(data.mDestX, data.mDestY);
-        }
-    }
-
-    @Override
-    public void scrollToEnd(
-        ReactNestedScrollView scrollView,
-        ReactScrollViewCommandHelper.ScrollToEndCommandData data) {
-      // ScrollView always has one child - the scrollable area
-      int bottom =
-        scrollView.getChildAt(0).getHeight() + scrollView.getPaddingBottom();
-      if (data.mAnimated) {
-        scrollView.smoothScrollTo(scrollView.getScrollX(), bottom);
-      } else {
-        scrollView.scrollTo(scrollView.getScrollX(), bottom);
-      }
-    }
-
-    @Override
-    public @Nullable Map getExportedCustomDirectEventTypeConstants() {
-        return createExportedCustomDirectEventTypeConstants();
-    }
-
-    public static Map createExportedCustomDirectEventTypeConstants() {
-        return MapBuilder.builder()
-            .put(ScrollEventType.SCROLL.getJSEventName(), MapBuilder.of("registrationName", "onScroll"))
-            .put(ScrollEventType.BEGIN_DRAG.getJSEventName(), MapBuilder.of("registrationName", "onScrollBeginDrag"))
-            .put(ScrollEventType.END_DRAG.getJSEventName(), MapBuilder.of("registrationName", "onScrollEndDrag"))
-            .put(ScrollEventType.ANIMATION_END.getJSEventName(), MapBuilder.of("registrationName", "onScrollAnimationEnd"))
-            .put(ScrollEventType.MOMENTUM_BEGIN.getJSEventName(), MapBuilder.of("registrationName", "onMomentumScrollBegin"))
-            .put(ScrollEventType.MOMENTUM_END.getJSEventName(), MapBuilder.of("registrationName", "onMomentumScrollEnd"))
-            .build();
-    }
+  public static Map createExportedCustomDirectEventTypeConstants() {
+    return MapBuilder.builder()
+        .put(ScrollEventType.SCROLL.getJSEventName(), MapBuilder.of("registrationName", "onScroll"))
+        .put(ScrollEventType.BEGIN_DRAG.getJSEventName(), MapBuilder.of("registrationName", "onScrollBeginDrag"))
+        .put(ScrollEventType.END_DRAG.getJSEventName(), MapBuilder.of("registrationName", "onScrollEndDrag"))
+        .put(ScrollEventType.ANIMATION_END.getJSEventName(), MapBuilder.of("registrationName", "onScrollAnimationEnd"))
+        .put(ScrollEventType.MOMENTUM_BEGIN.getJSEventName(), MapBuilder.of("registrationName", "onMomentumScrollBegin"))
+        .put(ScrollEventType.MOMENTUM_END.getJSEventName(), MapBuilder.of("registrationName", "onMomentumScrollEnd"))
+        .build();
+  }
 }
